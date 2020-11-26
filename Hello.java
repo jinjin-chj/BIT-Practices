fe1e4a1ad5bd5c9b82a29ad4ce8ceed7599436a3
jinjin-chj <75013048+jinjin-chj@users.noreply.github.com>
2020-11-26 13:57:04 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+	public static void main(String[] args) {
+		//System.out.println(3.4e3);	//3.4 x 10^3 표현
+		//System.out.println("I am a programmer~!");
+		long num1 = 3000000007L;
+		int num2 = (int)num1;	//명시적형변환
+		System.out.println(num2);
+	}
+}
