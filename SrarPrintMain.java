fa8784396a51be289a91056f3dbee87578e6fd87
jinjin-chj <75013048+jinjin-chj@users.noreply.github.com>
2020-12-01 20:17:07 +0900

    Update SrarPrintMain.java

--- SrarPrintMain.java
+++ SrarPrintMain.java
@@ -3,12 +3,11 @@ public class SrarPrintMain {
 
 	public static void main(String[] args) {
 		StarPrint starprint = new StarPrint();
-		starprint.setNum(3);
-		starprint.setNum(5);
 		
-		System.out.print(starprint.printTriangle());
+		starprint.printTriangle(3);
 		System.out.println();
-		System.out.print(starprint.printReverseTriangle());
+		starprint.printReverseTriangle(3);
+
 
 	}
 
