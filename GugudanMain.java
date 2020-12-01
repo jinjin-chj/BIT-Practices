9cc5728e25706e64533e78f47f36a3d7734e3e9a
jinjin-chj <75013048+jinjin-chj@users.noreply.github.com>
2020-12-01 20:16:27 +0900

    Update GugudanMain.java

--- GugudanMain.java
+++ GugudanMain.java
@@ -3,9 +3,9 @@ public class GugudanMain {
 
 	public static void main(String[] args) {
 		Gugudan gugudan = new Gugudan();
-		gugudan.setNum(7);
 		
-		System.out.println(gugudan.printGugu());
+		gugudan.printGugu(7);
+	
 	}
 
 }
