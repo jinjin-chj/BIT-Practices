443c9cb80df6907b482de87b6b4cd53d49214cf0
jinjin-chj <75013048+jinjin-chj@users.noreply.github.com>
2020-12-11 10:58:22 +0900

    Update TheaterArr.java

--- TheaterArr.java
+++ TheaterArr.java
@@ -6,58 +6,54 @@ class TheaterArr {
 
 	private final int ROWS = 10;
 	private final int COLS = 10;
-	private int[] seat1;
-	private int[] seat2;
+	private int[] seatLine1;
+	private int[] seatLine2;
 
 	public TheaterArr() {
-
-		this.seat1 = new int[ROWS];
-		this.seat2 = new int[COLS];
+		this.seatLine1 = new int[ROWS];
+		this.seatLine2 = new int[COLS];
 	}
 
 	private void input() {
-
-		for (int i = 0; i < seat1.length; i++) {
-			seat1[i] = i;
+		for (int i = 0; i < seatLine1.length; i++) {
+			seatLine1[i] = i;
 		}
-		for (int i = 0; i < seat2.length; i++) {
-			seat2[i] = 0;
+		for (int i = 0; i < seatLine2.length; i++) {
+			seatLine2[i] = 0;
 		}
-
 	}
 
 	private void seatPrint() {
 		System.out.println("-------------------");
-		for (int i = 0; i < seat1.length; i++) {
+		for (int i = 0; i < seatLine1.length; i++) {
 
-			System.out.print(seat1[i] + " ");
+			System.out.print(seatLine1[i] + " ");
 		}
 		System.out.println();
 		System.out.println("-------------------");
-		for (int i = 0; i < seat2.length; i++) {
-			System.out.print(seat2[i] + " ");
+		for (int i = 0; i < seatLine2.length; i++) {
+			System.out.print(seatLine2[i] + " ");
 		}
 		System.out.println();
-
 	}
 
 	private void Ticketing() {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("¸î¹øÂ° ÁÂ¼®À» ¿¹¾à ÇÏ½Ã°Ú½À´Ï±î? ");
+		System.out.print("ëª‡ë²ˆì§¸ ì¢Œì„ì„ ì˜ˆì•½ í•˜ì‹œê² ìŠµë‹ˆê¹Œ? ");
 		int myseat = sc.nextInt();
 
 		System.out.println("-------------------");
-		for (int i = 0; i < seat1.length; i++) {
+		for (int i = 0; i < seatLine1.length; i++) {
 
-			System.out.print(seat1[i] + " ");
+			System.out.print(seatLine1[i] + " ");
 		}
 		System.out.println();
 		System.out.println("-------------------");
-		for (int i = 0; i < seat2.length; i++) {
-			if (myseat == seat1[i]) {
-				seat2[i] = 1;
+		for (int i = 0; i < seatLine2.length; i++) {
+			if (myseat == seatLine1[i]) {
+				seatLine2[i] = 1;
 			}
-			System.out.print(seat2[i] + " ");
+			System.out.print(seatLine2[i] + " ");
 		}
 		sc.close();
 	}
