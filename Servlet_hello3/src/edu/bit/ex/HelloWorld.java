fae6066845db929fdfdf9e3db79c3ead98bfcf36
jinjin-chj <75013048+jinjin-chj@users.noreply.github.com>
2020-12-29 18:15:52 +0900

    Add files via upload

---
+++ Servlet_hello3/src/edu/bit/ex/HelloWorld.java
@@ -0,0 +1,70 @@
+package edu.bit.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hw")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HelloWorld~~");
+		// http://192.168.0.94:8282/servlet_hello3/hw
+		response.setContentType("text/html; charset=euc-kr");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h1>~~~~~GET 방식입니다.</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+		writer.close();
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld~~");
+		// http://192.168.0.94:8282/servlet_hello3/hw
+		response.setContentType("text/html; charset=euc-kr");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h1>doPost 방식입니다.</h1>");
+		writer.println("</body>");
+		writer.println("</html>");
+		
+		writer.close();
+	
+	}
+
+}
