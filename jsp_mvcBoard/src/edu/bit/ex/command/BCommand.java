d04b0fe234720e79e9de1d314a579bb62473883f
CHOIHYUNJIN <75013048+jinjin-chj@users.noreply.github.com>
2021-01-14 19:26:29 +0900

    Delete jsp_mvcBoard directory

--- jsp_mvcBoard/src/edu/bit/ex/command/BCommand.java
+++
@@ -1,8 +0,0 @@
-package edu.bit.ex.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BCommand {	// 게시판의 리스트 가져오는 것
-	abstract void execute(HttpServletRequest request, HttpServletResponse response);
-}
